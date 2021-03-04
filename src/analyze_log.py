587c66907dbab2e6842556319336d7cc277b1c25
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-03-04 19:25:17 -0300

    req 1 refactored

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,40 +1,64 @@
 import csv
-from collections import defaultdict
+from collections import defaultdict, Counter
+
+
+class Order:
+    def __init__(self):
+        self.dishes = Counter()
+        self.weekdays = Counter()
+
+    def add(self, dish, weekday):
+        self.dishes[dish] += 1
+        self.weekdays[weekday] += 1
+
+
+class TxtWriter:
+    def __init__(self, lines):
+        self.lines = [TxtWriter.add_sufix(line) for line in lines]
+
+    @staticmethod
+    def add_sufix(txt):
+        if not isinstance(txt, str):
+            txt = str(txt)
+        return txt + ";\n"
+
+    def save_file(self, path):
+        with open(path, "w", encoding="utf-8") as file:
+            file.writelines(self.lines)
+
+
+def extract_restaurant_info(path_to_file):
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"No such file or directory: {path_to_file}")
+
+    with open(path_to_file, encoding="utf-8") as file:
+        data = list(csv.reader(file))
+
+    orders = defaultdict(Order)
+    info = defaultdict(set)
+
+    for name, dish, weekday in data:
+        orders[name].add(dish, weekday)
+        info["all_dishes"].add(dish)
+        info["all_weekdays"].add(weekday)
+
+    return [orders, info]
 
 
 def analyze_log(path_to_file):
-    with open(path_to_file, encoding='utf-8') as file:
-        data = list(csv.reader(file))
-    all_dishes = set()
-    all_weekdays = set()
-    orders = defaultdict(lambda: {'dishes': defaultdict(int), 'weekdays': defaultdict(int)})
+    orders, info = extract_restaurant_info(path_to_file)
 
-    maria_dish = None
+    joao_dishes = set(orders["joao"].dishes.keys())
+    joao_weekdays = set(orders["joao"].weekdays.keys())
 
-    for name, dish, weekday in data:
-        this_order = orders[name]
-        dishes = this_order['dishes']
-        dishes[dish] += 1
-        this_order['weekdays'][weekday] += 1
-        all_dishes.add(dish)
-        all_weekdays.add(weekday)
-        if (name == 'maria' and dishes[dish] > dishes[maria_dish] or not maria_dish):
-            maria_dish = dish
+    writer = TxtWriter([
+            orders['maria'].dishes.most_common(1)[0][0],
+            orders["arnaldo"].dishes["hamburguer"],
+            info["all_dishes"] - joao_dishes,
+            info["all_weekdays"] - joao_weekdays,
+    ])
+    writer.save_file('data/mkt_campaign.txt')
 
-    lines = []
-    def append_line(txt):
-        if not isinstance(txt, str):
-            txt = str(txt)
-        lines.append(txt + ';\n')
-    append_line(maria_dish)
-    append_line(orders['arnaldo']['dishes']['hamburguer'])
 
-    joao_dishes = set(orders['joao']['dishes'].keys())
-    append_line(all_dishes - joao_dishes)
-    joao_weekdays = set(orders['joao']['weekdays'].keys())
-    append_line(all_weekdays - joao_weekdays)
 
-    with open('data/mkt_campaign.txt', 'w', encoding='utf-8') as file:
-        file.writelines(lines)
-
-analyze_log('./data/orders_1.csv')
+analyze_log("./data/orders_1.csv")
