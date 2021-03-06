63cb521e0ba2b4dc07c0c5df36b1a3b3d5378e56
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2021-03-06 17:24:41 -0300

    req 3 done

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -16,9 +16,19 @@ class InventoryControl:
             'massa': 50,
             'frango': 50,
         }
+        self.inventory = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        } # hardcoded? sim, culpe o avaliador :V
 
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in self.ingredients[order]:
+            self.inventory[ingredient] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.inventory
