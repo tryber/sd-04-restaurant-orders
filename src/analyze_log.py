f823ffc51c0e566950af06ee3e92a0553f4c1fcb
ribaslucas <lucas@mundoribas.com.br>
2021-03-08 15:49:07 -0300

    ribaslucas

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,17 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(
+            "No such file or directory: " f"'{path_to_file}'"
+        )
+
+    orders = list()
+
+    with open(path_to_file) as csv_file:
+        orders_reader = csv.reader(csv_file, delimiter=",")
+        for order in orders_reader:
+            orders.append(order)
+
+        csv_file.close
