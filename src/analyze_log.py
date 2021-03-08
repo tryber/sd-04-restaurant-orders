c686c178ca79d9b5ff24c2f98625b4f57d3f4a03
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-08 18:27:58 -0300

    25478

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -7,9 +7,16 @@ def read_the_file(path):
         data_raw = csv.DictReader(csvfile,
                                   fieldnames=['name', 'food', 'weekday'])
         for row in data_raw:
-            data.append({'name': row['name'],
-                        'food': row['food'], 'weekday': row['weekday']})
+            data.append([row['name'], row['food'],row['weekday']])
         return data
+
+
+def file_to_dic(orders):
+    data = [] 
+    for order in orders:
+        data.append({'name': order[0],
+        'food': order[1], 'weekday': order[2]})
+    return data
 
 
 def extract_from_orders(orders, data):
@@ -27,30 +34,33 @@ def count_customer_habits_set(customer_name, habit, habits_list, orders):
     return customer
 
 
-def count_customer_habits(customer_name, habit, habits_list, orders):
+def count_customer_habits(customer_name, habit, file_readd):
+    orders = file_to_dic(file_readd)
+    habits_list = extract_from_orders(orders, habit)
     customer = {}
     for one_habit in habits_list:
         customer[one_habit] = 0
     for order in orders:
         if order['name'] == customer_name:
             customer[order[habit]] += 1
-    return customer
+    max_ordered = max(customer, key=customer .get)
+    return max_ordered
 
 
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
     arnaldo_hamburger_ordered = 0
-    orders = read_the_file(path_to_file)
+    file_readd = read_the_file(path_to_file)
+    orders = file_to_dic(file_readd)
     weekday = extract_from_orders(orders, 'weekday')
     foods = extract_from_orders(orders, 'food')
-    maria_food = count_customer_habits("maria", 'food', foods, orders)
+    max_ordered = count_customer_habits("maria", 'food', file_readd)
     joao_food = count_customer_habits_set("joao", 'food', foods, orders)
     joao_day = count_customer_habits_set("joao", 'weekday', weekday, orders)
     for order in orders:
         if order['name'] == 'arnaldo' and order['food'] == 'hamburguer':
             arnaldo_hamburger_ordered += 1
-    max_ordered = max(maria_food, key=maria_food .get)
     with open("data/mkt_campaign.txt", mode="w") as file:
         file.write(f"{max_ordered}\n")
         file.write(f"{arnaldo_hamburger_ordered}\n")
