08adc948a9412093d63fc3418db98b086bc73d89
joaosoutto <joaosoutto@hotmail.com>
2021-03-08 19:26:20 -0300

    req 02 ok

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -3,7 +3,8 @@
 import operator
 import csv
 
-# Função para prato mais pedido --------------------------------------------------------
+
+# Função para prato mais pedido -----------------------------------------
 def get_favorite_meal(data, person):
     person_meals = {}
 
@@ -17,7 +18,7 @@ def get_favorite_meal(data, person):
     return max(person_meals.items(), key=operator.itemgetter(1))[0]
 
 
-# Função quantidade de vezes que o prato foi pedido--------------------------------------
+# Função quantidade de vezes que o prato foi pedido-----------------------
 def get_meal_qty(data, person, order):
     qty = 0
 
@@ -28,7 +29,7 @@ def get_meal_qty(data, person, order):
     return qty
 
 
-# Função para pratos NÃO pedidos --------------------------------------------------------
+# Função para pratos NÃO pedidos -----------------------------------------
 def get_unordered_meals(data, person):
     all_meals = set()
     person_meals = set()
@@ -42,7 +43,7 @@ def get_unordered_meals(data, person):
     return all_meals.difference(person_meals)
 
 
-# Função para dias NÃO idos -------------------------------------------------------------
+# Função para dias NÃO idos ---------------------------------------------
 def get_ungone_days(data, person):
     all_days = set()
     gone_days = set()
@@ -56,12 +57,10 @@ def get_ungone_days(data, person):
     return all_days.difference(gone_days)
 
 
-
-
 def analyze_log(path_to_file):
     with open(path_to_file, "r") as file:
         content = csv.reader(file, delimiter=",")
-        data = [*content]
+        data = list(content)
 
     with open("data/mkt_campaign.txt", "w") as analyze_file:
         analyze_file.write(f"{get_favorite_meal(data, 'maria')}\n")
