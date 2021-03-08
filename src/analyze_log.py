48c7224a6ec14d5386de25eaf998a95df9e4ed9b
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-07 21:30:13 -0300

    my first commit

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,5 @@
 import csv
-import operator
+
 
 def analyze_log(path_to_file):
     food = []
@@ -10,7 +10,6 @@ def analyze_log(path_to_file):
     arnaldo_hamburger_ordered = 0
     joao_food_not_ordered = set()
     joao_weekday_not_ordered = set()
-    data_list = []
 
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
@@ -20,31 +19,32 @@ def analyze_log(path_to_file):
             maria_food[food_item] = 0
             joao_food[food_item] = 0
 
-        data = csv.DictReader(csvfile, fieldnames = ['name', 'food', 'weekday'])
+        data = csv.DictReader(csvfile,
+        fieldnames = ['name', 'food', 'weekday'])
 
         for row in data:
             if row['food'] not in food:
-                food.append(row['food'] )
-                maria_food[ row['food'] ] = 0
-                joao_food[ row['food'] ] = 0
+                food.append(row['food'])
+                maria_food[row['food']] = 0
+                joao_food[row['food']] = 0
             if row['weekday'] not in weekday:
-                weekday.append(row['weekday'] )
-                joao_weekday[ row['weekday'] ] = 0
+                weekday.append(row['weekday'])
+                joao_weekday[row['weekday']] = 0
             if row['name'] == 'maria':
-                maria_food[ row['food'] ] += 1
+                maria_food[ row['food']] += 1
             if row['name'] == 'joao':
-                joao_food[ row['food'] ] += 1
+                joao_food[ row['food']] += 1
             if row['name'] == 'joao':
-                joao_weekday[ row['weekday'] ] += 1
+                joao_weekday[row['weekday']] += 1
             if row['name'] == 'arnaldo' and row['food'] == 'hamburguer':
                 arnaldo_hamburger_ordered += 1
 
-        max_ordered = max(maria_food , key=maria_food .get)
+        max_ordered = max(maria_food, key=maria_food .get)
         for food_item in food:
-            if joao_food[food_item] is 0:
+            if joao_food[food_item] == 0:
                 joao_food_not_ordered.add(food_item)
         for weekday_item in weekday:
-            if joao_weekday[weekday_item] is 0:
+            if joao_weekday[weekday_item] == 0:
                 joao_weekday_not_ordered.add(weekday_item)
 
         print("maria max ordered", max_ordered)
