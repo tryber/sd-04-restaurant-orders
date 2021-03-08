eaae9ee50e00991e4ac0809df263762ea3aef393
mathieu bouhelier <mathieubouhelier@gmail.com>
2021-03-07 22:35:11 -0300

    my first commit 222222222222222222222222

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,61 +1,94 @@
 import csv
 
+def read_the_file(path):
+    data = []
+    with open(path, encoding="utf-8") as csvfile:
+        data_raw = csv.DictReader(csvfile,
+        fieldnames = ['name', 'food', 'weekday'])
+
+        for row in data_raw:
+            data.append({'name': row['name'], 'food': row['food'], 'weekday': row['weekday']})
+        return data
+
+
+def extract_from_orders(orders, data):
+    list_to_extract = set()
+    for order in orders:
+        list_to_extract.add(order[data])
+    return list_to_extract
+
+
+def count_customer_habits (customer_name, habit, habits_list, orders):
+    customer = {}
+    for one_habit in habits_list:
+        customer[one_habit] = 0
+    print("customer", customer)
+    for order in orders:
+        if order['name'] == customer_name:
+            customer[order[habit]] += 1
+            customer['hamburguer'] += 1
+    return customer
+
 
 def analyze_log(path_to_file):
-    food = []
-    weekday = []
+    if not path_to_file.endswith(".csv"):
+        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+
     maria_food = {}
     joao_food = {}
     joao_weekday = {}
     arnaldo_hamburger_ordered = 0
     joao_food_not_ordered = set()
     joao_weekday_not_ordered = set()
+    data = []
+    dic = {}
 
-    if not path_to_file.endswith(".csv"):
-        raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
-    with open(path_to_file, encoding="utf-8") as csvfile:
+    orders = read_the_file(path_to_file)
+    weekday = extract_from_orders(orders, 'weekday')
+    foods = extract_from_orders(orders, 'food')
+    print("weekday", weekday, foods)
 
-        for food_item in food:
-            maria_food[food_item] = 0
-            joao_food[food_item] = 0
+    # for food in foods:
+    #     maria_food[food] = 0
 
-        data = csv.DictReader(csvfile,
-        fieldnames = ['name', 'food', 'weekday'])
+    # for order in orders:
+    #     if order['name'] == 'maria':
+    #         maria_food[order['food']] += 1
 
-        for row in data:
-            if row['food'] not in food:
-                food.append(row['food'])
-                maria_food[row['food']] = 0
-                joao_food[row['food']] = 0
-            if row['weekday'] not in weekday:
-                weekday.append(row['weekday'])
-                joao_weekday[row['weekday']] = 0
-            if row['name'] == 'maria':
-                maria_food[ row['food']] += 1
-            if row['name'] == 'joao':
-                joao_food[ row['food']] += 1
-            if row['name'] == 'joao':
-                joao_weekday[row['weekday']] += 1
-            if row['name'] == 'arnaldo' and row['food'] == 'hamburguer':
-                arnaldo_hamburger_ordered += 1
+    maria_food = count_customer_habits ("maria", 'food', foods, orders)
+    print('maria_food', maria_food)
 
-        max_ordered = max(maria_food, key=maria_food .get)
-        for food_item in food:
-            if joao_food[food_item] == 0:
-                joao_food_not_ordered.add(food_item)
-        for weekday_item in weekday:
-            if joao_weekday[weekday_item] == 0:
-                joao_weekday_not_ordered.add(weekday_item)
+    #         if row['food'] not in food:
+    #             food.append(row['food'])
+    #             maria_food[row['food']] = 0
+    #             joao_food[row['food']] = 0
+    #         if row['weekday'] not in weekday:
+    #             weekday.append(row['weekday'])
+    #             joao_weekday[row['weekday']] = 0
+    #         if row['name'] == 'maria':
+    #             maria_food[ row['food']] += 1
+    #         if row['name'] == 'joao':
+    #             joao_food[ row['food']] += 1
+    #         if row['name'] == 'joao':
+    #             joao_weekday[row['weekday']] += 1
+    #         if row['name'] == 'arnaldo' and row['food'] == 'hamburguer':
+    #             arnaldo_hamburger_ordered += 1
 
-        print("maria max ordered", max_ordered)
-        print("arnaldo_hamburger_ordered", arnaldo_hamburger_ordered)
-        print('joao_food_not_ordered', joao_food_not_ordered)
-        print('joao_weekday_not_ordered_list', joao_weekday_not_ordered)
+    max_ordered = max(maria_food, key=maria_food .get)
+    #     for food_item in food:
+    #         if joao_food[food_item] == 0:
+    #             joao_food_not_ordered.add(food_item)
+    #     for weekday_item in weekday:
+    #         if joao_weekday[weekday_item] == 0:
+    #             joao_weekday_not_ordered.add(weekday_item)
+
+    print("maria max ordered", max_ordered)
+    #     print("arnaldo_hamburger_ordered", arnaldo_hamburger_ordered)
+    #     print('joao_food_not_ordered', joao_food_not_ordered)
+    #     print('joao_weekday_not_ordered_list', joao_weekday_not_ordered)
 
     with open("data/mkt_campaign.txt", mode="w") as file:
         file.write(f"{max_ordered}\n")
-        file.write(f"{arnaldo_hamburger_ordered}\n")
-        file.write(f"{joao_food_not_ordered}\n")
-        file.write(f"{joao_weekday_not_ordered}")
-
-
+    #     file.write(f"{arnaldo_hamburger_ordered}\n")
+    #     file.write(f"{joao_food_not_ordered}\n")
+    #     file.write(f"{joao_weekday_not_ordered}")
