29dc005b6dc13751ad7e2487d475f77804fd936d
thiagooo14 <thiagooo15@gmail.com>
2021-03-09 02:30:26 -0300

    req 1 done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -6,66 +6,66 @@ def organized_list(read_orders):
     products = set()
     days_of_week = set()
 
-    for name, item, day in read_orders:
+    for client, item, day in read_orders:
         products.add(item)
         days_of_week.add(day)
-        if name not in order_list:
-            order_list[name] = [
+        if client not in order_list:
+            order_list[client] = [
                 {"product": item, "days_of_week": day}
             ]
         else:
-            order_list[name].append(
+            order_list[client].append(
                 {"product": item, "days_of_week": day}
             )
 
     return order_list, products, days_of_week
 
 
-def read_files_csv(path_to_file):
-    with open(path_to_file) as file:
-        reader_csv = csv.reader(file, delimiter=',', quotechar='"')
+def read_csv(path):
+    with open(path, mode="r") as file:
+        reader_csv = csv.reader(file, delimiter=",", quotechar='"')
         orders_list = organized_list(reader_csv)
     return orders_list
 
 
-def most_request(orders_list, client):
-    requested = ""
+def most_request(order_list, client):
+    requested = order_list[client][0]["product"]
     count = {}
-    for order in orders_list:
-        if order == client:
-            if order not in count:
-                count[order] = 1
-            else:
-                count[order] += 1
-            if count[order] > count[requested]:
-                requested = count[order]
-    return request
+    for order in order_list[client]:
+        if order["product"] not in count:
+            count[order["product"]] = 1
+        else:
+            count[order["product"]] += 1
+        if count[order["product"]] > count[requested]:
+            requested = order["product"]
+
+    return requested
 
 
 def count_asked(order_list, client, food):
     count = 0
     for order in order_list[client]:
-        if order == item:
+        if order["product"] == food:
             count += 1
 
     return count
 
 
-never_ordered(order_list, client, list_of, term):
+def never_ordered(order_list, client, list_of, term):
     dishes = set()
     client_orders = set(list_of)
-
-    for order in orders[client]:
+    for order in order_list[client]:
         dishes.add(order[term])
-    return (set_list.difference(products))
+
+    return (client_orders.difference(dishes))
 
 
 def analyze_log(path_to_file):
     order_list, products, days = read_csv(path_to_file)
 
-    with open("data/mkt_campaing.txt", mode="w") as file:
+    with open("data/mkt_campaign.txt", mode="w") as file:
         file.write(f"{most_request(order_list, 'maria')}\n")
-        file.write(f"{count_asked(order_list, 'arnaldo', 'hamburger')}\n")
+        file.write(f"{count_asked(order_list, 'arnaldo', 'hamburguer')}\n")
         file.write(
             f"{never_ordered(order_list, 'joao', products, 'product')}\n")
         file.write(
