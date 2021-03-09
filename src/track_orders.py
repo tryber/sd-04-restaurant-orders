9bffb031c904beef3b3a62858981019c2e1f21e5
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-03-08 22:50:24 -0300

    Track orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,66 @@
 class TrackOrders:
+
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        most_requested = ""
+        orders = {}
+
+        for name, order, day in self.orders:
+            if name == costumer:
+                if order not in orders:
+                    orders[order] = 1
+                else:
+                    orders[order] += 1
+
+                if (
+                    most_requested not in orders
+                    or orders[most_requested] < orders[order]
+                ):
+                    most_requested = order
+
+        return most_requested
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        total_quantity = 0
+
+        for name, order, day in self.orders:
+            if name == costumer and order == order:
+                total_quantity += 1
+
+        return total_quantity
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        products_costumers = set()
+        products = set()
+
+        for name, order, day in self.orders:
+            products.add(order)
+            if name == costumer:
+                products_costumers.add(order)
+
+        result = products.difference(products_costumers)
+        return result
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        visited_days = set()
+
+        for name, order, day in self.orders:
+            days.add(day)
+
+            if name == costumer:
+                visited_days.add(day)
+
+        result = days.difference(visited_days)
+        return result
 
     def get_busiest_day(self):
         pass
