08e779feac2d41c6f3ef67402d8d9dc72ac419a3
juniomelos <juniomelos@gmail.com>
2021-03-09 19:24:15 -0300

    test_validar_pedido_nunca_feito_pelo_cliente PASSED e test_validar_prato_mais_pedido PASSED

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -2,18 +2,17 @@ import operator
 import csv
 
 
-def ordered_dish_per_costumer(data, person):
+def ordered_dish_per_costumer(data, costumer):
     costumer_meals = {}
 
     for name, meal, day in data:
-        if person == name:
+        if costumer == name:
             if meal not in costumer_meals:
                 costumer_meals[meal] = 0
             else:
                 costumer_meals[meal] += 1
     max_ordered = max(costumer_meals.items(), key=operator.itemgetter(1))[0]
     return max_ordered
-
 
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
@@ -25,3 +24,4 @@ def analyze_log(path_to_file):
 
     with open("data/mkt_campaign.txt", "w") as analyze_file:
         analyze_file.write(f"{ordered_dish_per_costumer(data, 'maria')}\n")
+        analyze_file.write(f"{get_unordered_meals(data, 'joao')}\n")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,4 +1,6 @@
-# from src import analyze_log
+# from analyze_log import ordered_dish_per_costumer
+# from analyze_log import order_frequency_per_costumer
+from collections import defaultdict
 
 
 class TrackOrders:
@@ -14,19 +16,39 @@ class TrackOrders:
         return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        if len(self.orders) == 0:
+            return None
+        ordered_dish = defaultdict(int)
+        for order in self.orders:
+            if order[0] == costumer:
+                ordered_dish[order[1]] += 1
+        result = dict(ordered_dish)
+        return max(result, key=result.get)
 
-    def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+    # def get_order_frequency_per_costumer(self, costumer, order):
+    #     pass
+
+    def get_dishes(self):
+        dishes = set()
+        for order in self.orders:
+            dishes.add(order[1])
+        return dishes
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        if len(self.orders) == 0:
+            return None
+        ordered = set()
+        dishes = self.get_dishes()
+        for order in self.orders:
+            if order[0] == costumer:
+                ordered.add(order[1])
+        return dishes.difference(ordered)
 
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+    # def get_days_never_visited_per_costumer(self, costumer):
+    #     pass
 
-    def get_busiest_day(self):
-        pass
+    # def get_busiest_day(self):
+    #     pass
 
-    def get_least_busy_day(self):
-        pass
+    # def get_least_busy_day(self):
+    #     pass
