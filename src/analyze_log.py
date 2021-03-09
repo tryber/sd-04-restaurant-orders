e7bf2b80a4a65884e04a35bbc8214c0c3a3fe8f9
Fabiola Deiro <fabioladeiro7@gmail.com>
2021-03-08 22:30:04 -0300

    Again

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,11 +1,11 @@
 import csv
 
 
-def most_requested_recipe(orders, costumer):
-    favorite = ""
+def most_requested_recipe(total_orders, costumer):
+    most_requested = ""
     orders = {}
 
-    for name, order, day in orders:
+    for name, order, day in total_orders:
         if name == costumer:
             if order not in orders:
                 orders[order] = 1
@@ -13,20 +13,22 @@ def most_requested_recipe(orders, costumer):
                 orders[order] += 1
 
             if (
-                favorite not in orders
-                or orders[favorite] < orders[order]
+                most_requested not in orders
+                or orders[most_requested] < orders[order]
             ):
-                favorite = order
+                most_requested = order
 
-    return favorite
+    return most_requested
 
 
 def quantity_orders(orders, costumer, product):
-    quantity = 0
-    for order in orders[costumer]:
-        if order["product"] == product:
-            quantity += 1
-    return quantity
+    total_quantity = 0
+
+    for name, order, day in orders:
+        if name == costumer and order == product:
+            total_quantity += 1
+
+    return total_quantity
 
 
 def not_ordered(orders, costumer):
@@ -35,11 +37,11 @@ def not_ordered(orders, costumer):
 
     for name, order, day in orders:
         products.add(order)
-
         if name == costumer:
             products_costumers.add(order)
 
-    return products.difference(products_costumers)
+    result = products.difference(products_costumers)
+    return result
 
 
 def not_visited(orders, costumer):
@@ -52,27 +54,26 @@ def not_visited(orders, costumer):
         if name == costumer:
             visited_days.add(day)
 
-    return days.difference(visited_days)
+    result = days.difference(visited_days)
+    return result
 
 
 def analyze_log(path_to_file):
     with open(path_to_file, "r") as orders_file:
-        orders_reader = csv.reader(orders_file, delimiter=",")
-        orders = [*orders_reader]
-
-        most_requested_recipe_save = most_requested_recipe(orders, "maria")
-        quantity_orders_save = quantity_orders(
+        orders = [*csv.reader(orders_file, delimiter=",")]
+        most_requested_recipe_final = most_requested_recipe(orders, "maria")
+        quantity_orders_final = quantity_orders(
             orders, "arnaldo", "hamburguer"
         )
-        not_ordered_save = not_ordered(orders, "joao")
-        not_visited_save = not_visited(orders, "joao")
+        not_ordered_final = not_ordered(orders, "joao")
+        not_visited_final = not_visited(orders, "joao")
 
         with open("data/mkt_campaign.txt", "w") as marketing_list:
             print(
-                most_requested_recipe_save,
-                quantity_orders_save,
-                not_ordered_save,
-                not_visited_save,
+                most_requested_recipe_final,
+                quantity_orders_final,
+                not_ordered_final,
+                not_visited_final,
                 sep="\n",
                 file=marketing_list,
             )
