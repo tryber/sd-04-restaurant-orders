a41b71019b3c13fb4f97c0dc4861f3526d5fa157
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-03-09 23:26:22 -0300

    open file

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,11 @@
+import csv
+
+
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"No such file or directory: '{path_to_file}'")
+
+    with open(path_to_file, "r") as file:
+        content = csv.reader(file, delimiter=",")
+
+    print(content)
