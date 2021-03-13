6b78f5adcadf59ed97c27963c1ca3f38d933b992
Estela Martins <48722255+tamagoshii@users.noreply.github.com>
2021-03-13 19:39:39 -0300

    Req 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,21 +1,66 @@
 class TrackOrders:
+    def __init__(self):
+        self.list_orders = []
+
     def __len__(self):
-        pass
+        return len(self.list_orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        '''add novo pedido'''
+        self.list_orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        ''' retornar o prato mais pedido'''
+        favorite_recipe = ""
+        obj_orders = {}
+
+        for name, order, day in self.list_orders:
+            if name == costumer:
+                if order not in obj_orders:
+                    obj_orders[order] = 1
+                else:
+                    obj_orders[order] += 1
+
+                if (
+                    favorite_recipe not in obj_orders
+                    or obj_orders[order] > obj_orders[favorite_recipe]
+                ):
+                    favorite_recipe = order
+
+        return favorite_recipe
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        qty = 0
+
+        for name, day, recipe in self.list_orders:
+            if name == costumer and recipe == order:
+                qty += 1
+
+        return qty
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        recipes = set()
+        client_recipes = set()
+
+        for name, order, day in self.list_orders:
+            recipes.add(order)
+
+            if name == costumer:
+                client_recipes.add(order)
+
+        return recipes.difference(client_recipes)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        days = set()
+        client_visited = set()
+
+        for name, order, day in self.list_orders:
+            days.add(day)
+
+            if name == costumer:
+                client_visited.add(day)
+
+        return days.difference(client_visited)
 
     def get_busiest_day(self):
         pass
