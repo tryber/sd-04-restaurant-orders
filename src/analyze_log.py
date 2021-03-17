80f1e3dfd768adbae46bcfca618d88e202457da1
Tiago-ers <trsilva284@gmail.com>
2021-03-17 15:37:56 -0300

    refatorando 2

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -36,13 +36,14 @@ def analyze_log(path_to_file):
 
     # Prato mais pedido por maria
     maria_count_food = Counter(conj_maria['maria'])
-    maria_max_food = max(maria_count_food, key=maria_count_food.get)
-    # print(maria_max_food)
+    # maria_max_food = max(maria_count_food, key=maria_count_food.get)
+    # teste = list(maria_count_food.keys())[0]
+    # print(list(maria_count_food)[0])
 
     # Quantas vezes 'arnaldo' pediu 'hamburguer'
     arnaldo_count_food = Counter(conj_arnaldo['arnaldo'])
-    arnaldo_qtd_hamburguer = arnaldo_count_food['hamburguer']
-    # print(arnaldo_qtd_hamburguer)
+    # arnaldo_qtd_hamburguer = arnaldo_count_food['hamburguer']
+    # print(list(arnaldo_count_food.values())[1])
 
     # Quais pratos 'joao' nunca pediu
     joao_food = conj_food.difference(conj_joao['joao'])
@@ -53,9 +54,7 @@ def analyze_log(path_to_file):
     # criando arquivo csv
     with open('../data/mkt_campaign.txt', 'w') as writer_csv:
         writer_csv = csv.writer(writer_csv, delimiter=';')
-        writer_csv.writerow([maria_max_food])
-        writer_csv.writerow([arnaldo_qtd_hamburguer])
+        writer_csv.writerow([list(maria_count_food)[0]])
+        writer_csv.writerow([list(arnaldo_count_food.values())[1]])
         writer_csv.writerow([joao_food])
         writer_csv.writerow([joao_day])
-
-# analyze_log('../data/orders_1.csv')
