3133e983bd8d8a6ce4027259e2df635a70161564
Tiago-ers <trsilva284@gmail.com>
2021-03-17 14:57:44 -0300

    corrigindo formatação

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -7,6 +7,9 @@ def analyze_log(path_to_file):
     # caminho csv
     file = path_to_file
 
+    if not file.endswith(".csv"):
+        raise ValueError("Formato invalido")
+
     # criando conjuntos por pessoa,
     # por tipo de comida e dia da semana
     conj_arnaldo = defaultdict(list)
@@ -15,8 +18,6 @@ def analyze_log(path_to_file):
     conj_food = set()
     con_day = set()
 
-    if not file.endswith(".csv"):
-        raise ValueError("Formato invalido")
     # separando o csv por conjuntos
     with open(file) as f:
         file_text = csv.reader(f, delimiter=",", quotechar='"')
@@ -27,21 +28,21 @@ def analyze_log(path_to_file):
             # conjuntos por pessoa
             if name == 'maria':
                 conj_maria['maria'].append(food)
-            elif name == 'joao':
+            if name == 'joao':
                 conj_joao['joao'].append(food)
                 conj_joao['joao'].append(day)
-            elif name == 'arnaldo':
+            if name == 'arnaldo':
                 conj_arnaldo['arnaldo'].append(food)
 
     # Prato mais pedido por maria
     maria_count_food = Counter(conj_maria['maria'])
     maria_max_food = max(maria_count_food, key=maria_count_food.get)
-    print(maria_max_food)
+    # print(maria_max_food)
 
     # Quantas vezes 'arnaldo' pediu 'hamburguer'
     arnaldo_count_food = Counter(conj_arnaldo['arnaldo'])
     arnaldo_qtd_hamburguer = arnaldo_count_food['hamburguer']
-    print(arnaldo_qtd_hamburguer)
+    # print(arnaldo_qtd_hamburguer)
 
     # Quais pratos 'joao' nunca pediu
     joao_food = conj_food.difference(conj_joao['joao'])
@@ -56,5 +57,5 @@ def analyze_log(path_to_file):
         writer_csv.writerow([arnaldo_qtd_hamburguer])
         writer_csv.writerow([joao_food])
         writer_csv.writerow([joao_day])
-        
+
 # analyze_log('../data/orders_1.csv')
