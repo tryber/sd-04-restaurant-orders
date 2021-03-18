210b75c8d78b1c8315a3d58ed889f17bd279cfcb
Ronald Lima <ronaldazevedolima@gmail.com>
2021-03-18 19:27:17 -0300

    req 3 +-

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,5 +1,7 @@
 class InventoryControl:
     def __init__(self):
+        self.orders = []
+
         self.ingredients = {
             'hamburguer': ['pao', 'carne', 'queijo'],
             'pizza': ['massa', 'queijo', 'molho'],
@@ -17,8 +19,36 @@ class InventoryControl:
             'frango': 50,
         }
 
+    def get_available_dishes(self):
+        pratos = set()
+        ing_0 = set()
+
+        for ing in self.minimum_inventory:
+            if ing == 0:
+                ing_0.add(ing)
+                print(f"ing_0000000000000000000000000000, {ing_0}")
+
+        for prato in self.ingredients:
+            for ing in self.ingredients[prato]:
+                if ing not in ing_0:
+                    pratos.add(prato)
+                else:
+                    pratos.remove(prato)
+
+        return pratos
+
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
+
+        ingredientes = self.ingredients[order]
+        for ingrediente in ingredientes:
+            self.minimum_inventory[ingrediente] -= 1
 
     def get_quantities_to_buy(self):
-        pass
+        pedido = dict()
+        for ing in self.minimum_inventory:
+            if ing == 'queijo':
+                pedido[ing] = 100 - self.minimum_inventory[ing]
+            else:
+                pedido[ing] = 50 - self.minimum_inventory[ing]
+        return pedido
