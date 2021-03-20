c0337240f7075a266181d92871a1b88087d34a78
Tiago-ers <trsilva284@gmail.com>
2021-03-20 00:50:30 -0300

    req. 2 start

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,18 +1,31 @@
+from collections import defaultdict
+from collections import Counter
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = defaultdict(list)
+        self.foods = set()
+        
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
-
+        self.orders[costumer].append([order, day])
+        self.foods.add(order)
+        
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        costumer_most_ordered = Counter(self.orders[costumer][0])
+        return list(costumer_most_ordered.keys())[0]
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+       pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_food = set()
+        for item in self.orders[costumer]:
+            costumer_food.add(item[0])
+        return self.foods.difference(costumer_food)
 
     def get_days_never_visited_per_costumer(self, costumer):
         pass
