67840709d69b724be18cf98e08cf1af693fac39d
Tiago-ers <trsilva284@gmail.com>
2021-03-21 15:40:14 -0300

    req. 3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,19 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in self.ingredients[order]:
+            self.inventory[ingredient] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.inventory
