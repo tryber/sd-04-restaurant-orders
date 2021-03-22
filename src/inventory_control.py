9bd218205d67bf7c5c29d3724144d86b6864404c
Victor L <victor.lustosacp@gmail.com>
2021-03-22 15:44:33 -0300

    req3

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,5 +1,10 @@
+from collections import Counter
+
+
 class InventoryControl:
     def __init__(self):
+        self.orders_list = []
+
         self.ingredients = {
             'hamburguer': ['pao', 'carne', 'queijo'],
             'pizza': ['massa', 'queijo', 'molho'],
@@ -18,7 +23,24 @@ class InventoryControl:
         }
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders_list.append([costumer, order, day])
+        array = []
+        control = self.ingredients[order]
+        for ingre in control:
+            array.append(ingre)
+        counter_ingre = Counter(array)
+        for ingre in counter_ingre:
+            self.minimum_inventory[ingre] = self.minimum_inventory[ingre] - \
+                counter_ingre[ingre]
+        return True
 
     def get_quantities_to_buy(self):
-        pass
+        compras=dict()
+        for ingre in self.minimum_inventory:
+            if ingre == 'queijo':
+                compras[ingre]=100 - \
+                    self.minimum_inventory[ingre]
+            else:
+                compras[ingre]=50 - \
+                    self.minimum_inventory[ingre]
+        return compras
