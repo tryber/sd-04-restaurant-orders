81be1c0bd9aa9f89c48b67701419dbc154f4eecf
henrique <hbaetaleite@gmail.com>
2021-03-22 17:59:34 -0300

    tentando req 1 e 2

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,25 +1,74 @@
+from collections import defaultdict
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = list()
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        print(day)
-        pass
+        return self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        if len(self.orders) == 0:
+            return None
+        pedidos_lanche = defaultdict(int)
+
+        for order in self.orders:
+            if order[0] == costumer:
+                pedidos_lanche[order[1]] += 1
+
+        return max(pedidos_lanche, key=pedidos_lanche.get)
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        if len(self.orders) == 0:
+            return None
+        ordered = set()
+        dishes = set()
+        for order in self.orders:
+            dishes.add(order[1])
+
+        for order in self.orders:
+            if order[0] == costumer:
+                ordered.add(order[1])
+
+        return dishes.difference(ordered)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        if len(self.orders) == 0:
+            return None
+
+        day_clients = set()
+        todos_os_dias = set()
+
+        for order in self.orders:
+            todos_os_dias.add(order[2])
+
+        for order in self.orders:
+            if order[0] == costumer:
+                day_clients.add(order[2])
+
+        return todos_os_dias.difference(day_clients)
 
     def get_busiest_day(self):
-        pass
+        if len(self.orders) == 0:
+            return None
+        dias = defaultdict(int)
+        for order in self.orders:
+            dias[order[2]] += 1
+
+        return max(dias, key=dias.get)
 
     def get_least_busy_day(self):
-        pass
+        if len(self.orders) == 0:
+            return None
+        dias = defaultdict(int)
+        for order in self.orders:
+            dias[order[2]] += 1
+
+        return min(dias, key=dias.get)
