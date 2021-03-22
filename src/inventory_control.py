abf25f4c0d380e9c02e6ec5317e868c76e8b0666
danielPantalena <pantalenadaniel@gmail.com>
2021-03-22 18:23:55 -0300

    100%

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,22 +17,27 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
         self.orders_list = list()
 
     def add_new_order(self, costumer, order, day):
-        return self.orders_list.append([costumer, order, day])
+        if order not in self.get_available_dishes():
+            return False
+
+        for ingredient in self.ingredients[order]:
+            self.inventory[ingredient] += 1
 
     def get_quantities_to_buy(self):
-        ingredients_to_buy = {}
-        for ingredient in self.minimum_inventory.keys():
-            ingredients_to_buy[ingredient] = 0
-        for costumer, order, day in self.orders_list:
-            for ingredient in self.ingredients[order]:
-                if ingredient not in ingredients_to_buy:
-                    ingredients_to_buy[ingredient] = 1
-                else:
-                    ingredients_to_buy[ingredient] += 1
-        return ingredients_to_buy
+        return self.inventory
 
     def get_ingredients_not_available(self):
         not_available_ingredients = set()
@@ -41,4 +46,12 @@ class InventoryControl:
         return not_available_ingredients
     
     def get_available_dishes(self):
-        return set(self.ingredients.keys())
+        available_dishes = set(self.ingredients.keys())
+        for dish, ingredients in self.ingredients.items():
+            for ingredient in ingredients:
+                minimum_of_ingredient = self.minimum_inventory[ingredient]
+                if minimum_of_ingredient <= self.inventory[ingredient]:
+                    if dish in available_dishes:
+                        available_dishes.remove(dish)
+        return available_dishes
+
