d791d9713563c4c285ddb7e0fcf9e386a8fdff36
danielPantalena <pantalenadaniel@gmail.com>
2021-03-22 17:27:02 -0300

    Implement inventory_control

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,20 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.orders_list = list()
+
     def add_new_order(self, costumer, order, day):
-        pass
+        return self.orders_list.append([costumer, order, day])
 
     def get_quantities_to_buy(self):
-        pass
+        ingredients_to_buy = {}
+        for ingredient in self.minimum_inventory.keys():
+            ingredients_to_buy[ingredient] = 0
+        for costumer, order, day in self.orders_list:
+            for ingredient in self.ingredients[order]:
+                if ingredient not in ingredients_to_buy:
+                    ingredients_to_buy[ingredient] = 1
+                else:
+                    ingredients_to_buy[ingredient] += 1
+        return ingredients_to_buy
+        
