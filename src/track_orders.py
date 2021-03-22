d6d23056123af25300604a992cbfeab029c2fcb8
danielPantalena <pantalenadaniel@gmail.com>
2021-03-22 17:04:40 -0300

    Implement days frequence

--- src/track_orders.py
+++ src/track_orders.py
@@ -15,8 +15,12 @@ class TrackOrders:
     def get_most_ordered_dish_per_costumer(self, costumer):
         return get_food_most_requested_by_costumer(self.orders, costumer)
 
-    def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+    def get_order_frequency_per_costumer(self, costumer_param, order_param):
+        number_of_times = 0
+        for costumer, order, day in self.orders:
+            if costumer == costumer_param and order == order_param:
+                number_of_times += 1
+        return number_of_times
 
     def get_never_ordered_per_costumer(self, costumer):
         return get_foods_never_asked(self.orders, costumer)
@@ -25,7 +29,19 @@ class TrackOrders:
         return get_days_never_went(self.orders, costumer)
 
     def get_busiest_day(self):
-        pass
+        days_dict = dict()
+        for costumer, order, day in self.orders:
+            if day in days_dict:
+                days_dict[day] += 1
+            else:
+                days_dict[day] = 1
+        return max(days_dict, key=days_dict.get)
 
     def get_least_busy_day(self):
-        pass
+        days_dict = dict()
+        for costumer, order, day in self.orders:
+            if day in days_dict:
+                days_dict[day] += 1
+            else:
+                days_dict[day] = 1
+        return min(days_dict, key=days_dict.get)
