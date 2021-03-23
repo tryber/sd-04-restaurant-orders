aa4bd5a103fa15438e145392046e609aa7a474c7
djonata-dias <diasdjonata@gmail.com>
2021-03-23 09:14:07 -0300

    Criei as funções da classe do TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,58 @@
+import operator
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = list()
+        self.customer_orders = dict()
+        self.foods = set()
+        self.weekdays = dict()
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.foods.add(order)
+        self.orders.append([costumer, order, day])
+
+        if costumer not in self.customer_orders:
+            self.customer_orders[costumer] = {
+                "foods": {order: 1}, "weekdays": {day: 1}}
+
+        if day not in self.weekdays:
+            self.weekdays[day] = 1
+        else:
+            self.weekdays[day] += 1
+
+
+        if day not in self.customer_orders[costumer]["weekdays"]:
+            self.customer_orders[costumer]["weekdays"][day] = 1
+        else:
+            self.customer_orders[costumer]["weekdays"][day] += 1
+
+        if order not in self.customer_orders[costumer]["foods"]:
+            self.customer_orders[costumer]["foods"][order] = 1
+        else:
+            self.customer_orders[costumer]["foods"][order] += 1
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return max(self.customer_orders[costumer]["foods"].items(),
+                   key=operator.itemgetter(1))[0]
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return self.customer_orders[costumer]["foods"][order]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return self.foods - (self.customer_orders[costumer]["foods"]).keys()
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumer_weekdays = {*self.customer_orders[costumer]["weekdays"]}
+        weekdays = {*self.weekdays}
+
+        return weekdays - costumer_weekdays
 
     def get_busiest_day(self):
-        pass
+        return max(self.weekdays.items(), key=operator.itemgetter(1))[0]
 
     def get_least_busy_day(self):
-        pass
+        return min(self.weekdays.items(), key=operator.itemgetter(1))[0]
