c91b790bfe618698d8dd3b7f09ac500da06c5c69
djonata-dias <diasdjonata@gmail.com>
2021-03-23 09:42:17 -0300

    initial implement to inventory control

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,34 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.buy_list = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
+
+        self.orders = list()
+
     def add_new_order(self, costumer, order, day):
-        pass
+        ingredients = self.ingredients[order]
+        for ingredient in ingredients:
+            self.minimum_inventory[ingredient] -= 1
+            if ingredient not in self.buy_list:
+                self.buy_list[ingredient] = 1
+            else:
+                self.buy_list[ingredient] += 1
+
+        self.orders.append([costumer, order, day])
+
+        for item in self.minimum_inventory:
+            print(item)
 
     def get_quantities_to_buy(self):
+        return self.buy_list
+
+    def get_available_dishes():
         pass
