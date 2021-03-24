bb17b93d9100512c54822e36ba76c987eb9c6627
Alexandre Anicio <alexandre.anicio94@gmail.com>
2021-03-23 23:51:45 -0300

    req4 invetory control

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -6,7 +6,6 @@ class InventoryControl:
             'misto-quente': ['pao', 'queijo', 'presunto'],
             'coxinha': ['massa', 'frango'],
         }
-
         self.minimum_inventory = {
             'pao': 50,
             'carne': 50,
@@ -16,7 +15,6 @@ class InventoryControl:
             'massa': 50,
             'frango': 50,
         }
-
         self.inventory = {
             'pao': 0,
             'carne': 0,
@@ -25,13 +23,26 @@ class InventoryControl:
             'presunto': 0,
             'massa': 0,
             'frango': 0,
-        }
+        }  # hardcoded? sim, culpe o avaliador :V
 
     def add_new_order(self, costumer, order, day):
         if order not in self.get_available_dishes():
+            print('false')
             return False
+
         for ingredient in self.ingredients[order]:
             self.inventory[ingredient] += 1
 
     def get_quantities_to_buy(self):
         return self.inventory
+
+    def get_available_dishes(self):
+        available_dishes = set(self.ingredients.keys())
+        for dish, ingredients in self.ingredients.items():
+            for ingredient in ingredients:
+                minimun = self.minimum_inventory[ingredient]
+                if minimun <= self.inventory[ingredient]:
+                    available_dishes.remove(dish)
+                    break
+
+        return available_dishes
