5189aaf5fdcbda4003dbf4f948a8f4b173f61554
matheusysd <matheusyurisd@gmail.com>
2021-03-25 16:43:27 -0300

    Track orders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,38 @@
+from collections import defaultdict, Counter
+from src.order import Order
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = defaultdict(Order)
+        self.all_dishes = Counter()
+        self.all_weekdays = Counter()
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        print(f"{costumer}  {order}  {day}")
+        self.orders[costumer].add(order, day)
+        self.all_dishes[order] += 1
+        self.all_weekdays[day] += 1
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return self.orders[costumer].most_common_dish()
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        return self.orders[costumer].dishes[order]
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        costumer_activity = self.orders[costumer].dishes.keys()
+        return self.all_dishes.keys() - costumer_activity
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        costumer_activity = self.orders[costumer].weekdays.keys()
+        return self.all_weekdays.keys() - costumer_activity
 
     def get_busiest_day(self):
-        pass
+        return self.all_weekdays.most_common(1)[0][0]
 
     def get_least_busy_day(self):
-        pass
+        return self.all_weekdays.most_common()[-1][0]
