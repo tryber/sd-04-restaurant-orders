8aef463bc2d395dd67bc939b43e74c1ff4957047
mirellaalves <mirella.alves@gmail.com>
2021-03-26 00:57:24 -0300

    requirement 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -31,12 +31,28 @@ class InventoryControl:
         order_ingredients = self.ingredients[order]
 
         for ingredient in order_ingredients:
-            self.inventory[ingredient] -= 1
+            if self.inventory[ingredient] <= 0:
+                return False
+            else:
+                self.inventory[ingredient] -= 1
 
     def get_quantities_to_buy(self):
         ingredients_to_buy = {}
+
         for ingredient in self.inventory:
             ingredients_to_buy[ingredient] = (
                 self.minimum_inventory[ingredient] - self.inventory[ingredient]
             )
         return ingredients_to_buy
+
+    def get_available_dishes(self):
+        available_dishes = []
+
+        for dish, ingredients in self.ingredients.items():
+            available_dishes.append(dish)
+            for ingredient in ingredients:
+                if self.inventory[ingredient] <= 0:
+                    available_dishes.pop()
+                    break
+
+        return set(available_dishes)
