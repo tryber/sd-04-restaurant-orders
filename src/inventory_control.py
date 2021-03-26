91abcadf95f3978a35b9e9d5742e253c60ead377
gabrielukke <gabriellukk@outlook.com>
2021-03-26 20:18:33 -0300

    The Last One

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,34 @@ class InventoryControl:
             'frango': 50,
         }
 
+        self.inventory = {
+            'carne': 0,
+            'pao': 0,
+            'frango': 0,
+            'molho': 0,
+            'massa': 0,
+            'queijo': 0,
+            'presunto': 0,
+        }
+
     def add_new_order(self, costumer, order, day):
-        pass
+        for item in self.ingredients[order]:
+            if self.minimum_inventory[item] == 0:
+                return False
+            self.minimum_inventory[item] -= 1
+            if item in self.inventory:
+                self.inventory[item] += 1
+            else:
+                self.inventory[item] = 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.inventory
+
+    def get_available_dishes(self):
+        dishes = set(self.ingredients.keys())
+        for dish, ingredients in self.ingredients.items():
+            for ingredient in ingredients:
+                if self.minimum_inventory[ingredient] == 0:
+                    dishes.remove(dish)
+                    break
+        return dishes
