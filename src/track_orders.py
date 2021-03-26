5c99d613d9580027b9ebaf784d65b890d26ccf86
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-26 10:49:49 -0300

    tracker blank spaces

--- src/track_orders.py
+++ src/track_orders.py
@@ -3,14 +3,11 @@ class TrackOrders:
     def __init__(self):
         self.orders = []
 
-
     def __len__(self):
         return len(self.orders)
 
-
     def add_new_order(self, costumer, order, day):
         self.orders.append([costumer, order, day])
-
 
     def get_most_ordered_dish_per_costumer(self, costumer):
         request = []
@@ -23,14 +20,12 @@ class TrackOrders:
             great_bigger = dict(bigger)
         return great_bigger[max(great_bigger)]
 
-
     def get_order_frequency_per_costumer(self, costumer, order):
         requested = []
         for line in self.orders:
             if line[0] == costumer:
                 requested.append(line[1])
         return requested.count(order)
-
 
     def get_never_ordered_per_costumer(self, costumer):
         requested = set()
@@ -42,7 +37,6 @@ class TrackOrders:
                 requested.add(line[1])
         return requested - request_user
 
-
     def get_days_never_visited_per_costumer(self, costumer):
         days = set()
         days_user = set()
@@ -53,7 +47,6 @@ class TrackOrders:
                 days.add(line[2])
         return days - days_user
 
-
     def get_busiest_day(self):
         request = []
         bigger = set()
@@ -63,7 +56,6 @@ class TrackOrders:
             bigger.add((request.count(requested), requested))
             great_bigger = dict(bigger)
         return great_bigger[max(great_bigger)]
-
 
     def get_least_busy_day(self):
         request = []
