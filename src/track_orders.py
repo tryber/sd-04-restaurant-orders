d0ab3b4005103443a0032048738b88ed538ce270
gabrielukke <gabriellukk@outlook.com>
2021-03-26 20:08:22 -0300

    Req 2 - All tests

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,56 @@
+from src.analyze_log import never_ordered, never_visited
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        order = [costumer, order, day]
+        self.orders.append(order)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        orders = {}
+        for name, food, _ in self.orders:
+            if name == costumer:
+                if food not in orders:
+                    orders[food] = 1
+                else:
+                    orders[food] += 1
+        return max(orders, key=orders.get)
 
     def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+        count = 0
+        for name, food, _ in self.orders:
+            if name == costumer and food == order:
+                count += 1
+        return count
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
+        return never_ordered(self.orders, costumer)
 
     def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return never_visited(self.orders, costumer)
 
     def get_busiest_day(self):
-        pass
+        days_visited = {}
+
+        for _, _, day in self.orders:
+            if day is not days_visited:
+                days_visited[day] = 1
+            else:
+                days_visited[day] += 1
+        return max(days_visited, key=days_visited.get)
 
     def get_least_busy_day(self):
-        pass
+        days_visited = {}
+
+        for _, _, day in self.orders:
+            if day in days_visited:
+                days_visited[day] += 1
+            else:
+                days_visited[day] = 1
+        return min(days_visited, key=days_visited.get)
