bfbe755f189efbf7c9aa1730961fd095d7c54a91
Luma Arruda <luma.oarruda@gmail.com>
2021-03-26 12:30:00 -0300

    req bonus delete e add - passou local

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,4 +1,5 @@
 class InventoryControl:
+
     def __init__(self):
         self.ingredients = {
             'hamburguer': ['pao', 'carne', 'queijo'],
@@ -17,8 +18,43 @@ class InventoryControl:
             'frango': 50,
         }
 
-    def add_new_order(self, costumer, order, day):
-        pass
+        self.quantities_to_buy = {
+            'pao': 0,
+            'carne': 0,
+            'queijo': 0,
+            'molho': 0,
+            'presunto': 0,
+            'massa': 0,
+            'frango': 0,
+        }
 
-    def get_quantities_to_buy(self):
-        pass
+        self.all_recipes = set([
+            'hamburguer',
+            'pizza',
+            'misto-quente',
+            'coxinha',
+        ])
+
+    def remove_recipes(self, ingredient):
+        for recipes in self.ingredients:
+            qty_ingredients = set(self.ingredients[recipes])
+            if ingredient in qty_ingredients:
+                self.all_recipes.discard(recipes)
+
+    def add_new_order(self, costumer, order, day):
+        aux_ingredients = self.ingredients[order]
+        for ingredient in aux_ingredients:
+            if (
+                self.quantities_to_buy[ingredient] + 1 <
+                self.minimum_inventory[ingredient]
+            ):
+                self.quantities_to_buy[ingredient] += 1
+            elif (
+                self.quantities_to_buy[ingredient] <
+                self.minimum_inventory[ingredient]
+            ):
+                self.quantities_to_buy[ingredient] += 1
+                self.remove_recipes(ingredient)
+            else:
+                return False
+
