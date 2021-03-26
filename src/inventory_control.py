24157e216921b77d8d9b31a3328c601905cb7c34
Luma Arruda <luma.oarruda@gmail.com>
2021-03-26 12:06:55 -0300

    fiz tudo no arquivo errado

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,65 +1,24 @@
-from src.analyze_log import favorite_recipe
-from src.analyze_log import qty_orders
-from src.analyze_log import not_orders
-from src.analyze_log import get_days_client_gone
-
-
-class TrackOrders:
+class InventoryControl:
     def __init__(self):
-        self.orders = {}
-        self.products = set()
-        self.days_of_week = set()
-        self.orders_per_day = {}
-        self.lenght = 0
+        self.ingredients = {
+            'hamburguer': ['pao', 'carne', 'queijo'],
+            'pizza': ['massa', 'queijo', 'molho'],
+            'misto-quente': ['pao', 'queijo', 'presunto'],
+            'coxinha': ['massa', 'frango'],
+        }
 
-    def __len__(self):
-        return self.lenght
+        self.minimum_inventory = {
+            'pao': 50,
+            'carne': 50,
+            'queijo': 100,
+            'molho': 50,
+            'presunto': 50,
+            'massa': 50,
+            'frango': 50,
+        }
 
     def add_new_order(self, costumer, order, day):
-        if costumer not in self.orders:
-            self.orders[costumer] = [
-                {"product": order, "days_of_week": day}
-            ]
-        else:
-            self.orders[costumer].append(
-                {"product": order, "days_of_week": day}
-            )
+        pass
 
-        self.lenght += 1
-        self.products.add(order)
-
-        if day not in self.orders_per_day:
-            self.orders_per_day[day] = 1
-        else:
-            self.orders_per_day[day] += 1
-        self.days_of_week.add(day)
-
-    def get_most_ordered_dish_per_costumer(self, costumer):
-        return favorite_recipe(self.orders, costumer)
-
-    def get_dish_quantity_per_costumer(self, costumer, order):
-        return qty_orders(self.orders, costumer, order)
-
-    def get_never_ordered_per_costumer(self, costumer):
-        return not_orders(self.orders, costumer, self.products, "product")
-
-    def get_busiest_day(self):
-        frequency = list(self.orders_per_day.keys())[0]
-
-        for day in self.orders_per_day:
-            if self.orders_per_day[day] > self.orders_per_day[frequency]:
-                frequency = day
-
-        return frequency
-
-    def get_least_busy_day(self):
-        frequency = list(self.orders_per_day.keys())[0]
-
-        for day in self.orders_per_day:
-            if self.orders_per_day[day] < self.orders_per_day[frequency]:
-                frequency = day
-
-        return frequency
-
-    def get_days_never_visited_per_costumer(self, costumer):
-        return get_days_client_gone(self.orders, costumer)
+    def get_quantities_to_buy(self):
+        pass
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,24 +1,65 @@
+from src.analyze_log import favorite_recipe
+from src.analyze_log import qty_orders
+from src.analyze_log import not_orders
+from src.analyze_log import get_days_client_gone
+
+
 class TrackOrders:
+    def __init__(self):
+        self.orders = {}
+        self.products = set()
+        self.days_of_week = set()
+        self.orders_per_day = {}
+        self.lenght = 0
+
     def __len__(self):
-        pass
+        return self.lenght
 
     def add_new_order(self, costumer, order, day):
-        pass
+        if costumer not in self.orders:
+            self.orders[costumer] = [
+                {"product": order, "days_of_week": day}
+            ]
+        else:
+            self.orders[costumer].append(
+                {"product": order, "days_of_week": day}
+            )
+
+        self.lenght += 1
+        self.products.add(order)
+
+        if day not in self.orders_per_day:
+            self.orders_per_day[day] = 1
+        else:
+            self.orders_per_day[day] += 1
+        self.days_of_week.add(day)
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        return favorite_recipe(self.orders, costumer)
 
-    def get_order_frequency_per_costumer(self, costumer, order):
-        pass
+    def get_dish_quantity_per_costumer(self, costumer, order):
+        return qty_orders(self.orders, costumer, order)
 
     def get_never_ordered_per_costumer(self, costumer):
-        pass
-
-    def get_days_never_visited_per_costumer(self, costumer):
-        pass
+        return not_orders(self.orders, costumer, self.products, "product")
 
     def get_busiest_day(self):
-        pass
+        frequency = list(self.orders_per_day.keys())[0]
+
+        for day in self.orders_per_day:
+            if self.orders_per_day[day] > self.orders_per_day[frequency]:
+                frequency = day
+
+        return frequency
 
     def get_least_busy_day(self):
-        pass
+        frequency = list(self.orders_per_day.keys())[0]
+
+        for day in self.orders_per_day:
+            if self.orders_per_day[day] < self.orders_per_day[frequency]:
+                frequency = day
+
+        return frequency
+
+    def get_days_never_visited_per_costumer(self, costumer):
+        return get_days_client_gone(self.orders, costumer)
