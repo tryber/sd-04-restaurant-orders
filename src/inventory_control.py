18a37ba5a69a781efca81ee5554ed56b6ab551cb
CAIO RIBEIRO <cacobribeiro@gmail.com>
2021-03-25 23:50:29 -0300

    wat

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -17,8 +17,36 @@ class InventoryControl:
             'frango': 50,
         }
 
+
+        self.inventory = self.minimum_inventory.copy()
+
     def add_new_order(self, costumer, order, day):
-        pass
+        for ingredient in self.ingredients[order]:
+            if self.inventory[ingredient] == 0:
+                return False
+
+            self.inventory[ingredient] -= 1
+
 
     def get_quantities_to_buy(self):
-        pass
+         return dict([
+            (ingredient, self.minimum_inventory[ingredient]
+                - self.inventory[ingredient])
+
+            for ingredient in self.minimum_inventory
+        ])
+
+
+    def get_available_dishes(self):
+        return {
+            dish for dish in self.ingredients if self.is_dish_avaible(dish)
+        }
+
+
+    def is_dish_avaible(self, dish):
+        for ingredient in self.ingredients[dish]:
+            if self.inventory[ingredient] == 0:
+                return False
+
+        return True
+        
