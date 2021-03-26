5fcff4262d5971872b7e4517e1d1c7fa93a26e2a
Ilan Herbach <ilan.herbach@gmail.com>
2021-03-26 20:10:33 -0300

    New order and most ordered per costumer

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,12 +1,23 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, costumer, order, day):
-        pass
+        self.orders.append([costumer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, costumer):
-        pass
+        pedidos = []
+        maior = set()
+        for line in self.orders:
+            if line[0] == costumer:
+                pedidos.append(line[1])
+        for pedido in pedidos:
+            maior.add((pedidos.count(pedido), pedido))
+            maior2 = dict(maior)
+        return maior2[max(maior2)]
 
     def get_order_frequency_per_costumer(self, costumer, order):
         pass
