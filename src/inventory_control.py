f46e17a8db5ac9cf90dd5797ba22bb5e8d2f95ef
fogo5000 <lucaslucas2008@gmail.com>
2021-03-27 06:53:29 -0300

    Refact requisition 3 && 4

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,48 +1,50 @@
 class InventoryControl:
     def __init__(self):
+        self.orders = []
         self.ingredients = {
-            'hamburguer': ['pao', 'carne', 'queijo'],
-            'pizza': ['massa', 'queijo', 'molho'],
-            'misto-quente': ['pao', 'queijo', 'presunto'],
-            'coxinha': ['massa', 'frango'],
+            "hamburguer": ["pao", "carne", "queijo"],
+            "pizza": ["massa", "queijo", "molho"],
+            "misto-quente": ["pao", "queijo", "presunto"],
+            "coxinha": ["massa", "frango"],
         }
 
         self.minimum_inventory = {
-            'pao': 50,
-            'carne': 50,
-            'queijo': 100,
-            'molho': 50,
-            'presunto': 50,
-            'massa': 50,
-            'frango': 50,
+            "pao": 50,
+            "carne": 50,
+            "queijo": 100,
+            "molho": 50,
+            "presunto": 50,
+            "massa": 50,
+            "frango": 50,
         }
-
         self.inventory = {
-            'pao': 0,
-            'carne': 0,
-            'queijo': 0,
-            'molho': 0,
-            'presunto': 0,
-            'massa': 0,
-            'frango': 0,
+            "pao": 0,
+            "carne": 0,
+            "queijo": 0,
+            "molho": 0,
+            "presunto": 0,
+            "massa": 0,
+            "frango": 0,
         }
 
     def add_new_order(self, costumer, order, day):
+        self.orders.append([costumer, order, day])
         if order not in self.get_available_dishes():
             return False
 
-        for ingredient in self.ingredients[order]:
-            self.inventory[ingredient] = +1
+        for ing in self.ingredients[order]:
+            self.inventory[ing] += 1
 
     def get_quantities_to_buy(self):
         return self.inventory
 
     def get_available_dishes(self):
-        foods = set(self.ingredients.keys())
-        for food, ingredients in self.ingredients.items():
-            for ingredient in ingredients:
-                minimun = self.minimum_inventory[ingredient]
-                if minimun <= self.inventory[ingredient]:
-                    foods.remove(food)
+        available_ds = list(self.ingredients.keys())
+        for dish, ings in self.ingredients.items():
+            for ing in ings:
+                min_ing = self.minimum_inventory[ing]
+                if min_ing <= self.inventory[ing]:
+                    available_ds.remove(dish)
                     break
-        return set(foods)
+
+        return set(available_ds)
